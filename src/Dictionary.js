2d4a842ece589d18481f6d8a2dcfd887e2b4d067
Edeltraud K <edeltraud.kohl@email.de>
2024-03-31 23:27:02 +0200

    included an api url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
-    alert("Searching");
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=f08f864ef6a33151ee24fc3db63obtbb`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleResponse(response) {
+    console.log(response.data);
   }
 
   function handleKeywordChange(event) {
