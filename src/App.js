51744e47451bd26bf22eb3af7e52c16732e2c162
Edeltraud K <edeltraud.kohl@email.de>
2024-03-31 23:45:31 +0200

    Changed the Header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,6 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={process.env.PUBLIC_URL + "/easter-2319286_1280.jpg"}
-          className="App-logo"
-          alt="logo"
-        />
-      </header>
       <main>
         <Dictionary />
       </main>
