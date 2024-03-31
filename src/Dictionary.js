5bb5d30e9aca1981ca6309506aefafc1ace8dee1
Edeltraud K <edeltraud.kohl@email.de>
2024-03-31 23:42:00 +0200

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,13 +4,15 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [meanings, setMeanings] = useState([]);
+
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=f08f864ef6a33151ee24fc3db63obtbb`;
     axios.get(apiUrl).then(handleResponse);
   }
   function handleResponse(response) {
-    console.log(response.data);
+    setMeanings(response.data.meanings);
   }
 
   function handleKeywordChange(event) {
@@ -25,6 +27,24 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <div className="definition">
+        {meanings.map((meaning, index) => (
+          <div key={index}>
+            <h2>{index + 1} .</h2>
+            <p>Part of Speech: {meaning.partOfSpeech}</p>
+            <p>Definition: {meaning.definition}</p>
+            <p>Example: {meaning.example}</p>
+            <p>
+              Synonyms:{" "}
+              {meaning.synonyms ? meaning.synonyms.join(", ") : "None"}
+            </p>
+            <p>
+              Antonyms:{" "}
+              {meaning.antonyms ? meaning.synonyms.join(", ") : "None"}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
