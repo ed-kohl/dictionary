cb7662f3c95d33f8009d392d66b514e42327f3d8
Edeltraud K <edeltraud.kohl@email.de>
2024-03-31 22:39:38 +0200

    changed the pcture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
-import easter from "./easter-2319286_1280.jpg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={easter - 2319286_1280} className="App-logo" alt="logo" />
+        <img
+          src={process.env.PUBLIC_URL + "/easter-2319286_1280.jpg"}
+          className="App-logo"
+          alt="logo"
+        />
       </header>
       Dictionary
     </div>
