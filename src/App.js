45bdaf8825121ba064373a86415f15f4a06d6f01
Edeltraud K <edeltraud.kohl@email.de>
2024-03-31 22:30:29 +0200

    changed some content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import easter from "./easter-2319286_1280.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={easter - 2319286_1280} className="App-logo" alt="logo" />
       </header>
+      Dictionary
     </div>
   );
 }
