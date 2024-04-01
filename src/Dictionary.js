fe5efd4c93578bd8881b0b00d2125a7516d7e268
Edeltraud K <edeltraud.kohl@email.de>
2024-04-01 12:48:48 +0200

    Optimized the design

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
-import Imagedisplay from "./Imagedisplay.js";
 
-export default function Dictionary() {
+function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [meanings, setMeanings] = useState("");
 
@@ -28,29 +27,66 @@ export default function Dictionary() {
       <h1>Dictionary</h1>
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <button>Search</button>
       </form>
       <div className="definition">
         {Array.isArray(meanings) &&
           meanings.map((meaning, index) => (
             <div key={index}>
-              <h2>{index + 1} .</h2>
-              <p>Part of Speech: {meaning.partOfSpeech}</p>
-              <p>Definition: {meaning.definition}</p>
-              <p>Example: {meaning.example}</p>
-              <p>
-                Synonyms:{" "}
-                {meaning.synonyms ? meaning.synonyms.join(", ") : "None"}
-              </p>
-              <p>
-                Antonyms:{" "}
-                {meaning.antonyms ? meaning.antonyms.join(", ") : "None"}
-              </p>
-              <p>
-                <Imagedisplay />
-              </p>
+              <h3>{index + 1} .</h3>
+
+              <div className="results">
+                <p className="category">Definition: </p>
+                <p className="majordef">
+                  <b>
+                    {""}
+                    {Array.isArray(meaning.definition)
+                      ? meaning.definition.join(",")
+                      : meaning.definition || "None"}
+                  </b>
+                </p>
+              </div>
+              <div className="results">
+                <p className="category">Part of Speech: </p>
+                <p>
+                  {""}
+                  {Array.isArray(meaning.partOfSpeech)
+                    ? meaning.partOfSpeech.join(",")
+                    : meaning.partOfSpeech || "None"}
+                </p>
+              </div>
+              <div className="results">
+                <p className="category">Example: </p>
+                <p>
+                  {""}
+                  {Array.isArray(meaning.example)
+                    ? meaning.example.join(",")
+                    : meaning.example || "None"}
+                </p>
+              </div>
+              <div className="results">
+                <p className="category">Synonyms:</p>
+                <p>
+                  {""}
+                  {Array.isArray(meaning.synonyms)
+                    ? meaning.synonyms.join(",")
+                    : meaning.synonyms || "None"}
+                </p>
+              </div>
+              <div className="results">
+                <p className="category">Antonyms:</p>
+                <p>
+                  {""}
+                  {Array.isArray(meaning.antonyms)
+                    ? meaning.antonyms.join(",")
+                    : meaning.antonyms || "None"}
+                </p>
+              </div>
             </div>
           ))}
       </div>
     </div>
   );
 }
+
+export default Dictionary;
